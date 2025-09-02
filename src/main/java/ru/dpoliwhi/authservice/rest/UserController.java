ceed381f92b91b7bddc85016e9bd64772bb926c1
dpoliwhi <evdidenko93@gmail.com>
2025-09-02 23:39:33 +0700

    Add security config.

---
+++ src/main/java/ru/dpoliwhi/authservice/rest/UserController.java
@@ -0,0 +1,20 @@
+package ru.dpoliwhi.authservice.rest;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/me")
+    public Authentication whoAmI() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        return context.getAuthentication();
+    }
+}
