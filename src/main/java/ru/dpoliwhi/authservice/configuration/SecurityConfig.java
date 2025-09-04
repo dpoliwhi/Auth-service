aca244f2a2de173f5d1a0f457379092273364c15
dpoliwhi <evdidenko93@gmail.com>
2025-09-04 22:09:52 +0700

    get access for requests

--- src/main/java/ru/dpoliwhi/authservice/configuration/SecurityConfig.java
+++ src/main/java/ru/dpoliwhi/authservice/configuration/SecurityConfig.java
@@ -2,9 +2,9 @@ package ru.dpoliwhi.authservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,8 +14,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // Allow all requests without authentication
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
